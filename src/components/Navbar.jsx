ff0dfd01071d262e74bf7ac5bc290de518990b1f
Prabha Adhitya <prabhaadhitya01@gmail.com>
2026-02-23 11:27:10 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,10 @@ import { Link, useLocation } from "react-router-dom";
 function Navbar() {
   const location = useLocation();
 
-  const active = (path) => {
-    location.pathname === path 
-      ? "text-blue-500 font-semibold" 
-      : "text-slate-300 hover:text-blue-500"
-  }
+  const active = (path) =>
+    location.pathname === path
+      ? "text-blue-500 font-semibold"
+      : "text-slate-300 hover:text-blue-500";
 
   return (
     <header className='flex justify-between pt-15 pb-10 pl-20 pr-20 bg-[#0F172A]'>
@@ -15,13 +14,13 @@ function Navbar() {
         <h1 className='text-3xl text-white'>Github Profile Visualizer</h1>
       </Link>        
       <ul className='flex space-x-6'>          
-        <li className={active('/')}>
+        <li className={`${active('/')} text-xl cursor-pointer`}>
           <Link to="/">Home</Link>
         </li>
-        <li className={active('/repositories')}>
+        <li className={`${active('/repositories')} text-xl cursor-pointer`}>
           <Link to="/repositories">Repositories</Link>
         </li>
-        <li className={active('/analysis')}>
+        <li className={`${active('/analysis')} text-xl cursor-pointer`}>
           <Link to="/analysis">Analysis</Link>
         </li>          
       </ul>
